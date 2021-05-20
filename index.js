9ec35ecdbc4bbe494abd9512911d5b2c72799369
vineet-coder <vineetsinghal2312@gmail.com>
2021-05-21 01:39:05 +0530

    add dependency and port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port =process.env.PORT || 8000;
 const cors = require("cors");
 require("dotenv").config();
 const { initializeDbConnection } = require("./db/db.connection");
